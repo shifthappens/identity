3d28c6b4f02f203812aaad5923dff152d371a486
shifthappens <shifthappens@21207766-c656-4b57-b01a-933bce388b9c>
2009-10-06 23:08:58 +0000

    First initial commit of files becoming the very first shift website.

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Shifthappens.nl</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="css/screen.css" />
+		<link rel="stylesheet" type="text/css" media="print" href="css/print.css" />
+		<!--[if lte IE 7]>
+		<link rel="stylesheet" type="text/css" href="css/ie.css" media="screen, projection" />
+		<![endif]-->
+	</head>
+	<body>
+	<div class="container showgrid">
+		<div class="prepend-7 span-10 append-7 last">
+			<h1>Data!</h1>
+		</div>
+		
+		<div class="span-12">
+		<p>Lorem ipsum</p>
+		</div>
+		
+		<div class="span-12 last">
+		<p>Lorem ipsum 2</p>
+		</div>
+	</div>
+	</body>
+</html>
