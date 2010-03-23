fd0f0e64d8f3d4d14caac21cd60ec6c210694c2a
shifthappens <shifthappens@21207766-c656-4b57-b01a-933bce388b9c>
2010-03-23 22:28:42 +0000

    made some improvements to the temp page for BCD presentation.

--- index.php
+++ index.php
@@ -21,22 +21,22 @@
 				{
 					console.log("hello");
 									
-					var trans_body = new Effect.Morph('body', { style: 'background: #F2F7D4;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete.") } } );
+					var trans_body = new Effect.Morph('body', { style: 'background: #F2F7D4;', duration: 30, afterFinish: function() { console.log("transition to daytime complete.") } } );
 /*
-					$$('h1').each(function(event, index) { console.log("h1 transition starting..."); Effect.Morph(event, { style: 'color: #B6D903;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete (h1).") } } ) } );
-					$$('h2').each(function(event, index) { console.log(event); var effect = Effect.Morph(event.identify(), { style: 'color: #B6D903;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete (h2).") } } ) } );
+					$$('h1').each(function(event, index) { console.log("h1 transition starting..."); Effect.Morph(event, { style: 'color: #B6D903;', duration: 30, afterFinish: function() { console.log("transition to daytime complete (h1).") } } ) } );
+					$$('h2').each(function(event, index) { console.log(event); var effect = Effect.Morph(event.identify(), { style: 'color: #B6D903;', duration: 30, afterFinish: function() { console.log("transition to daytime complete (h2).") } } ) } );
 */
-					var effect = new Effect.Morph('h2-one', { style: 'color: #B6D903;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete (h1).") } } );
-					var effect = new Effect.Morph('h2-two', { style: 'color: #B6D903;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete (h2).") } } );
+					var effect = new Effect.Morph('h2-one', { style: 'color: #B6D903;', duration: 30, afterFinish: function() { console.log("transition to daytime complete (h1).") } } );
+					var effect = new Effect.Morph('h2-two', { style: 'color: #B6D903;', duration: 30, afterFinish: function() { console.log("transition to daytime complete (h2).") } } );
 
-					var effect = new Effect.Morph('sidebar', { style: 'color: #000000;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete (h1).") } } );
-					var effect = new Effect.Morph('content', { style: 'color: #000000;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete (h2).") } } );
+					var effect = new Effect.Morph('sidebar', { style: 'color: #000000;', duration: 30, afterFinish: function() { console.log("transition to daytime complete (h1).") } } );
+					var effect = new Effect.Morph('content', { style: 'color: #000000;', duration: 30, afterFinish: function() { console.log("transition to daytime complete (h2).") } } );
 
-					var trans_body = new Effect.Morph('body', { style: 'background: #F2F7D4;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete.") } } );
-					var trans_body = new Effect.Morph('container', { style: 'background: #F2F7D4;', duration: 3600, afterFinish: function() { console.log("transition to daytime complete.") } } );
+					var trans_body = new Effect.Morph('body', { style: 'background: #F2F7D4;', duration: 30, afterFinish: function() { console.log("transition to daytime complete.") } } );
+					var trans_body = new Effect.Morph('container', { style: 'background: #F2F7D4;', duration: 30, afterFinish: function() { console.log("transition to daytime complete.") } } );
 
-					var trans_body = new Effect.Morph('block-1', { style: 'background: #FFFFFF; background-image: url(images/text.png)', duration: 3600, afterFinish: function() { console.log("transition to daytime complete.") } } );
-					var trans_body = new Effect.Morph('block-2', { style: 'background: #FFFFFF; background-image: url(images/text.png)', duration: 3600, afterFinish: function() { console.log("transition to daytime complete.") } } );
+					var trans_body = new Effect.Morph('block-1', { style: 'background: #FFFFFF; background-image: url(images/text.png)', duration: 30, afterFinish: function() { console.log("transition to daytime complete.") } } );
+					var trans_body = new Effect.Morph('block-2', { style: 'background: #FFFFFF; background-image: url(images/text.png)', duration: 30, afterFinish: function() { console.log("transition to daytime complete.") } } );
 
 					console.log("body = " + $('body'));
 				});
